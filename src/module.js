496b2fbdeb8fd2b764ba253dd1b93829f36d271c
AndreyPanovA <panov3107@mail.ru>
2020-08-21 23:18:41 +0300

    rendering

--- src/module.js
+++ src/module.js
@@ -1,1 +1,13 @@
-console.log("module.js")
+export const model = [
+    {
+        type: "title", value: "test title", options: {}
+    },
+    {
+        type: "text", value: `Lorem ipsum dolor sit amet consectetur, adipisicing elit. Blanditiis, harum neque! Asperiores
+        corrupti eveniet amet, eum architecto eius perferendis aliquam.`, options: {}
+    },
+    { type: "textColumns", value: ["1 text", "2 text", "3 text", "4 text"], options: {} },
+    {
+        type: "title", value: "test title", options: {}
+    },
+]
