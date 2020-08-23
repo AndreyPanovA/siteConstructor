9871288c063533f85fa16c0cf7367a6c29301a42
AndreyPanovA <panov3107@mail.ru>
2020-08-23 20:09:32 +0300

    test Commot

--- src/module.js
+++ src/module.js
@@ -1,19 +1,25 @@
+const text = `
+Крутые видео и уроки по JavaScript тут. Тут ты найдешь исчерпывающую информацию по любым аспектам языка, любым фреймворкам, такие как: React, Vue, Angular, Node, Svelte, Express, Next, Nuxt и многое другое. Присоединяйся!
+`
+
+
 export const model = [
     {
-        type: "title", value: "Test title", options: {
-            styles: "text-align: center; background: darkred",
+        type: "title", value: 'Конструктор сайтов на чистом JavaScript!', options: {
+            styles: 'background: linear-gradient(to right, #ff0099, #493240);color: #fff;padding: 1.5rem;text-align: center;',
             tag: "h1"
         }
     },
     {
-        type: "text", value: `Lorem ipsum dolor sit amet consectetur, adipisicing elit. Blanditiis, harum neque! Asperiores
-        corrupti eveniet amet, eum architecto eius perferendis aliquam.`, options: {
-            styles: "text-align: center; background: #333; color: gold;"
-        }
+        type: "textColumns", value: ['Приложение на чистом JavaScript, без использования библиотек',
+            'Узнаешь как работают принципы SOLID и ООП в JavaScript за один курс',
+            'JavaScript - это просто, интересно. Научись создавать любые UI своими руками'], options: {
+                styles: 'background: linear-gradient(to left, #f2994a, #f2c94c);font-weight: bold;padding: 1rem;'
+            }
     },
     {
-        type: "textColumns", value: ["1 text", "2 text", "3 text", "4 text"], options: {
-            styles: "padding:1rem;"
+        type: "text", value: [text], options: {
+            styles: 'padding: 2rem 0; color: #fff;background: linear-gradient(to bottom, #8e2de2, #4a00e0);font-weight: bold;'
         }
     },
 ]
